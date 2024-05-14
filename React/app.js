2199f39d4d384f8b59f7186759eb75f977fa244d
Harshit-Sisodia <harshitsisodia789@gmail.com>
2024-05-14 14:04:25 +0530

    update

---
+++ React/app.js
@@ -0,0 +1,50 @@
+// const parent = document.getElementById('root');
+// const root = ReactDOM.createRoot(parent);
+
+// const heading = document.createElement('h1');
+
+// heading.innerText='hello from js dom';
+
+// const list = document.createElement('ul');
+// const listItem1= document.createElement('li');
+// listItem1.innerText = 'Item1';
+// const listItem2 = document.createElement('li');
+
+// list.appendChild(listItem1);
+// list.appendChild(listItem2);
+// parent.appendChild
+
+// const heading = React.createElement('h1',{},"Hello from react");
+
+// const li1 = React.createElement()
+
+// const li1 = React.createElement('li',{key: 'li1', className:},"Item1");
+// const li2 = React.createElement('li',{key:'li2'},"Item2");
+// const list = React.createElement('ul',{},[li1, li2]);
+
+// root.render(list);
+// root.render(heading);
+// console.log(heading)
+const ReactDOM = require('react-dom/client');
+
+const parent = document.getElementById('root');
+const root = ReactDOM.createRoot(parent);
+
+
+// const str='Hello from React!';
+const heading = React.createElement
+
+const li1 = <li>Item1</li>
+const li2 = <li>Item2</li>
+const arr= [li1,li2];
+const list = <ul>{li1}{li2}</ul>  
+
+
+
+root.render(list);
+
+
+
+
+root.render(heading);
+
